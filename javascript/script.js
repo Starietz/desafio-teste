9aff994d73d0eab544a15bc6adde60b031c543d0
Starietz <himizukbroken88@gmail.com>
2025-11-26 20:01:18 -0300

    desafio

--- javascript/script.js
+++ javascript/script.js
@@ -1,21 +1,24 @@
-
 let nome = "Emerson"
 let quantidadeXp = 5005
 
+let nivel = ""
+
 if (quantidadeXp < 1000) {
-    console.log("O herói de nome " + nome + " está no nível de Ferro")
-} else if (quantidadeXp >= 1001 && quantidadeXp <= 2000) {
-    console.log("O herói de nome " + nome + " está no nível de Bronze")
-} else if (quantidadeXp >= 2001 && quantidadeXp <= 5000) {
-    console.log("O herói de nome " + nome + " está no nível de Prata")
-} else if (quantidadeXp >= 5001 && quantidadeXp <= 7000) {
-    console.log("O herói de nome " + nome + " está no nível de Ouro")
-} else if (quantidadeXp >= 7001 && quantidadeXp <= 8000) {
-    console.log("O herói de nome " + nome + " está no nível de Platina")
-} else if (quantidadeXp >= 8001 && quantidadeXp <= 9000) {
-    console.log("O herói de nome " + nome + " está no nível de Ascendente")
-} else if (quantidadeXp >= 9001 && quantidadeXp <= 10000) {
-    console.log("O herói de nome " + nome + " está no nível de Imortal")
+    nivel = "Ferro"
+} else if (quantidadeXp <= 2000) {
+    nivel = "Bronze"
+} else if (quantidadeXp <= 5000) {
+    nivel = "Prata"
+} else if (quantidadeXp <= 7000) {
+    nivel = "Ouro"
+} else if (quantidadeXp <= 8000) {
+    nivel = "Platina"
+} else if (quantidadeXp <= 9000) {
+    nivel = "Ascendente"
+} else if (quantidadeXp <= 10000) {
+    nivel = "Imortal"
 } else {
-    console.log("O herói de nome " + nome + " está no nível de Radiante")
+    nivel = "Radiante"
 }
+
+console.log("O Herói de nome:" + nome + " está no nível de: "+ quantidadeXp)
