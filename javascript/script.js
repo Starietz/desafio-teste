287c34d100e4748fe0340fea005abd53a3f4773d
Starietz <himizukbroken88@gmail.com>
2025-11-26 19:36:38 -0300

    desafio

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,21 @@
 let nome = "Emerson"
+let quantidadeXp = 5005
 
-console.log("Seu nome é: " +nome)
+ if(quantidadeXp <= 1000){
+    console.log("Você é Ferro")
+ }else if (quantidadeXp >= 1001 && quantidadeXp <= 2000){
+    console.log("Você é Bronze")
+ }else if (quantidadeXp >= 2001 && quantidadeXp <= 5000 ){
+    console.log("Você é Prata")
+ }else if(quantidadeXp >= 5001 && quantidadeXp <= 7000){
+    console.log("Você é Ouro")
+ }else if(quantidadeXp >= 7001 && quantidadeXp <=8000){
+    console.log("Você é Platina")
+ }else if(quantidadeXp >= 8001 && quantidadeXp <= 9000){
+    console.log("Você é Ascendente")
+ }else if(quantidadeXp >= 9001 && quantidadeXp <= 10000){
+    console.log("Você é Imortal")
+ }else {
+    console.log("Você é Radiante")
+ }
+ console.log("O herói :" + nome + " está no nível " + quantidadeXp)
