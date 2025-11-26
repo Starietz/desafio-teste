f72dac385aef0889349dd4a551b6253145b2077b
Starietz <himizukbroken88@gmail.com>
2025-11-26 20:06:46 -0300

    desafio

--- javascript/script.js
+++ javascript/script.js
@@ -1,24 +1,23 @@
 let nome = "Emerson"
 let quantidadeXp = 5005
-
 let nivel = ""
 
-if (quantidadeXp < 1000) {
-    nivel = "Ferro"
-} else if (quantidadeXp <= 2000) {
-    nivel = "Bronze"
-} else if (quantidadeXp <= 5000) {
-    nivel = "Prata"
-} else if (quantidadeXp <= 7000) {
-    nivel = "Ouro"
-} else if (quantidadeXp <= 8000) {
-    nivel = "Platina"
-} else if (quantidadeXp <= 9000) {
-    nivel = "Ascendente"
-} else if (quantidadeXp <= 10000) {
-    nivel = "Imortal"
-} else {
-    nivel = "Radiante"
+if (quantidadeXp < 1001) {
+  nivel = "Ferro"
+} else if (quantidadeXp >= 1001 && quantidadeXp <= 2000) {
+  nivel = "Bronze"
+} else if (quantidadeXp >= 2001 && quantidadeXp <= 5000) {
+  nivel = "Prata"
+} else if (quantidadeXp >= 5001 && quantidadeXp <= 7000) {
+  nivel = "Ouro"
+} else if (quantidadeXp >= 7001 && quantidadeXp <= 8000) {
+  nivel = "Platina"
+} else if (quantidadeXp >= 8001 && quantidadeXp <= 9000) {
+  nivel = "Ascendente"
+} else if (quantidadeXp >= 9001 && quantidadeXp <= 10000) {
+  nivel = "Imortal"
+} else if (quantidadeXp >= 10001) {
+  nivel = "Radiante"
 }
 
-console.log("O Herói de nome:" + nome + " está no nível de: "+ nivel)
+console.log("O Herói de nome " + nome + " está no nível de " + nivel)
